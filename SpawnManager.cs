aab91bf4acd72b2b22ce408788c6831e4eb0ca31
Genzai <97627357+Genzai86@users.noreply.github.com>
2022-07-15 13:01:10 +0200

    Add files via upload

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -3,43 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
-{   // esto [] indica una array / matriz
+{
+    public GameObject[] animalPrefabs; //this [] indicates array
 
-    public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20;
-    private float spawnPosZ = 20;
-    private float spawnRangeLR = 20;
-    private float startDelay = 2;
-    private float spawnInterval = 1.5f;
+    public float startTime = 2;
+    public float spawnInterval = 1.5f;
+    public float spawnRangeLR = 15;
+
+    public float spawnRangeX = 10;
+    public float spawnPosZ = 27;
+
 
     // Start is called before the first frame update
     void Start()
-    {   
-        // Metodo en void Start para que empiece al comenzar la partida
-        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    {
+        //Invoke repeating is a function that calls on a method based on the time parameters you give it
+        //Arguments: ("Function", TimeStart, Interval)
+        InvokeRepeating("SpawnRandomAnimal", startTime, spawnInterval);
     }
 
-   
-    //Generamos otro metodo void.
     void SpawnRandomAnimal()
     {
-        //Se generan aleatoriamente desde animal index y spawn position. Generamos el animal para que aparezca desde arriba, derecha o izquierda.
+        //randomise spawn location
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-        Vector3 spawnPosLeft = new Vector3(-spawnRangeLR, 0, Random.Range(-5, 30));
-        Vector3 spawnPosRight = new Vector3(spawnRangeLR, 0, Random.Range(-5, 30));
+        Vector3 spawnPosLeft = new Vector3(-spawnRangeLR, 0, Random.Range(6, 20));
+        Vector3 spawnPosRight = new Vector3(spawnRangeLR, 0, Random.Range(6, 20));
 
-
-        //int = numeros enteros. Decimos que, animalIndex es un numero entero que es igual a un numero aleatorio en un rango que va desde 0 a toda la lista de animalprefabs.
-        //Creo 3 spawn para que cada lado cree packs diferentes de animales
-       
+        //randomly generate an animal to spawn
+        //I created 3 spawns here so that each side creates a different batch of animals
         int animalIndex = Random.Range(0, animalPrefabs.Length);
-        int animalIndex1 = Random.Range(0, animalPrefabs.Length);
         int animalIndex2 = Random.Range(0, animalPrefabs.Length);
+        int animalIndex3 = Random.Range(0, animalPrefabs.Length);
 
-        // ir al proyecto y comprobar el Gameobject que esta vacio en el que hay una lista.
-        // Quaternion para definir rotacion del objeto.
+        //actual piece of code spawning animals
+        //there are various ways to put arguments, but over here it's (object, Vector3 position, Quaterion.position)
         Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        Instantiate(animalPrefabs[animalIndex1], spawnPosLeft, Quaternion.Euler(0, 90, 0));
-        Instantiate(animalPrefabs[animalIndex2], spawnPosRight, Quaternion.Euler(0, -90, 0));
+
+        Instantiate(animalPrefabs[animalIndex2], spawnPosLeft, Quaternion.Euler(0, 90, 0));
+
+        Instantiate(animalPrefabs[animalIndex3], spawnPosRight, Quaternion.Euler(0, -90, 0));
+
     }
 }
